16a032e3ee145051bf5805bc86d3e432ab0c9b80
X-Line <ashilr11@gmail.com>
2022-10-02 12:49:49 +0200

    Added timer project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,7 @@ import harvester from "../assets/covers/harvester.png"
 import capstone from "../assets/covers/capstone.png"
 import restapi from "../assets/covers/bookshelf.jpg"
 import africasteel from "../assets/covers/africasteel.png"
+import timer from "../assets/covers/timer.png"
 
 export const projects = [
     {
@@ -29,5 +30,10 @@ export const projects = [
         name: "Bookshelf API",
         cover: restapi,
         link: "https://github.com/ashilramjee/bookshelf"
+    },
+    {
+        name: "Timer",
+        cover: timer,
+        link: "https://ashilramjee.com/timer"
     },
 ];
